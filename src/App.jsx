385327b0213c2ba2be9bca13bb42fae29b8fa4c1
msalonen <p33155@edu.sasky.fi>
2024-12-02 20:58:01 +0200

    lisää Header komponentin

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,21 @@ import './App.css'
 import Suitcase from './components/Suitcase';
 import Balance from './components/Balance';
 import Booster from './components/Booster';
+import Header from './components/Header';
 
 function App() {
 
   return (
     <>
       <div>
+        <Header>Travel clicker</Header>
         <Balance total="157" />
         <Suitcase />
         <Booster value="3.2" />
       </div>  
     </>
   )
+
 }
 
 export default App
