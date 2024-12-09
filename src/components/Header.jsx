b087f7cb693418677548ea30af0e7f658b2ff0d8
msalonen <p33155@edu.sasky.fi>
2024-12-09 13:35:48 +0200

    muuttaa Header-komponentin toiminnan

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,21 @@
+import iconSuitcase from '../assets/suitcase.svg';
+
 function Header(props) {
 
+  if (props.hasOwnProperty("balance")) {
+    return (
+      <div className="header header_sub">
+        <h1>{props.children}</h1>
+        <div>{props.balance} <img src={iconSuitcase}/></div>
+      </div>
+    );
+  } else {
     return (
       <div className="header">
         <h1>{props.children}</h1>
       </div>
     );
+  }
   
   }
   
