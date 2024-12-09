b6f3a4e7f6f24791899610562188806b0c1a9769
msalonen <p33155@edu.sasky.fi>
2024-12-09 13:30:56 +0200

    lisää linkityksen Menu-komponenttiin

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,23 +1,28 @@
 import iconSuitcase from '../assets/suitcase.svg';
 import iconPackage from '../assets/package.svg';
 import iconSettings from '../assets/settings.svg';
+import { NavLink } from "react-router-dom";
 
 function Menu(props) {
 
-  return (
-    <div className="menu">
-      <div>
-        <img src={iconSuitcase} alt="main" />
-      </div>
-      <div>
-        <img src={iconPackage} alt="store" />
-        { props.items ? <span className="menu_badge">{props.items}</span> : null }
-      </div>
-      <div>
-        <img src={iconSettings} alt="settings" />
-      </div>
-    </div>
-  );
+    return (
+        <div className="menu">
+          <div>
+            <NavLink to="/"><img src={iconSuitcase} alt="main" /></NavLink>
+          </div>
+          <div>
+            <NavLink to="/store">
+              <img src={iconPackage} alt="store" />
+              { props.items ? <span className="menu_badge">{props.items}</span> : null }
+            </NavLink>
+          </div>
+          <div>
+            <NavLink to="/settings"><img src={iconSettings} alt="settings" /></NavLink>
+          </div>
+        </div>
+      );
+    
+
   
 }
   
