5a5745873441431454bcf3544072178fcfee7599
msalonen <p33155@edu.sasky.fi>
2024-12-10 18:56:55 +0200

    lisää tuotekuvat

--- src/config/items.js
+++ src/config/items.js
@@ -1,128 +1,128 @@
 export default [
     { 
-      id: "lemontree",
-      name: "Lemon tree",
+      id: "sunglasses",
+      name: "Sunglasses",
       qty: 0,
       baseprice: 10,
       price: 10,
       multiplier: 0.2,
-      image: "lemontree.svg"
+      image: "sunglasses.svg"
     },
     { 
-      id: "blender",
-      name: "Blender",
+      id: "passport",
+      name: "Passport",
       qty: 0,
       baseprice: 70,
       price: 70,
       multiplier: 2,
-      image: "blender.svg"
+      image: "passport.svg"
     },
     { 
-      id: "carbonator",
-      name: "Carbonator",
+      id: "map",
+      name: "Map",
       qty: 0,
       baseprice: 490,
       price: 490,
       multiplier: 10,
-      image: "carbonator.svg"
+      image: "map.svg"
     },
     { 
-      id: "bottler",
-      name: "Bottler",
+      id: "plane-ticket",
+      name: "Plane ticket",
       qty: 0,
       baseprice: 3400,
       price: 3400,
       multiplier: 50,
-      image: "bottler.svg"
+      image: "plane-ticket.svg"
     },
     { 
-      id: "truck",
-      name: "Truck",
+      id: "eiffel-tower",
+      name: "Paris, France",
       qty: 0,
       baseprice: 24000,
       price: 24000,
       multiplier: 235,
-      image: "truck.svg"
+      image: "eiffel-tower.svg"
     },
     { 
-      id: "spring",
-      name: "Spring",
+      id: "volcano",
+      name: "Reykjavik, Iceland",
       qty: 0,
       baseprice: 169000,
       price: 169000,
       multiplier: 1150,
-      image: "spring.svg"
+      image: "volcano.svg"
     },
     { 
-      id: "sugarcane",
-      name: "Sugar cane field",
+      id: "pyramids",
+      name: "Cairo, Egypt",
       qty: 0,
       baseprice: 1200000,
       price: 1200000,
       multiplier: 5600,
-      image: "sugarcane.svg"
+      image: "pyramids.svg"
     },   
     { 
-      id: "warehouse",
-      name: "Warehouse",
+      id: "savannah",
+      name: "Nairobi, Kenya",
       qty: 0,
       baseprice: 8400000,
       price: 8400000,
       multiplier: 27500,
-      image: "warehouse.svg"
+      image: "savannah.svg"
     }, 
     { 
-      id: "lab",
-      name: "Lab",
+      id: "christ-the-redeemer",
+      name: "Rio de Janeiro, Brazil",
       qty: 0,
       baseprice: 59000000,
       price: 59000000,
       multiplier: 135000,
-      image: "lab.svg"
+      image: "christ-the-redeemer.svg"
     },
     { 
-      id: "secret",
-      name: "Secret ingredient",
+      id: "great-buddha",
+      name: "Bangkok, Thailand",
       qty: 0,
       baseprice: 417000000,
       price: 417000000,
       multiplier: 650000,
-      image: "secret.svg"
+      image: "great-buddha.svg"
     },
     {
-      id: "power",
-      name: "Power plant",
+      id: "llama",
+      name: "Cusco, Peru",
       qty: 0,
       baseprice: 2950000000,
       price: 2950000000,
       multiplier: 3200000,
-      image: "power.svg"
+      image: "llama.svg"
     },
     {
-      id: "park",
-      name: "Amusement park",
+      id: "taj-mahal",
+      name: "Agra, India",
       qty: 0,
       baseprice: 20800000000,
       price: 20800000000,
       multiplier: 15600000,
-      image: "park.svg"
+      image: "taj-mahal.svg"
     },
     {
-      id: "lemonai",
-      name: "LemonAI",
+      id: "kangaroo",
+      name: "Sydney, Australia",
       qty: 0,
       baseprice: 147000000000,
       price: 147000000000,
       multiplier: 76100000,
-      image: "lemonai.svg"
+      image: "kangaroo.svg"
     },
     {
-      id: "accelerator",
-      name: "Particle accelerator",
+      id: "island",
+      name: "Bora Bora, French Polynesia",
       qty: 0,
       baseprice: 1040000000000,
       price: 1040000000000,
       multiplier: 371000000,
-      image: "accelerator.svg"
+      image: "island.svg"
     },
   ];
